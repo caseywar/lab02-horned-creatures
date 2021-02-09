799ae37f7023d5d1371c1cf4b3a531b2ceca7a9a
Casey Warren <caseywar1017@gmail.com>
2021-02-09 18:36:12 -0500

    basic app structure built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { images } from './data.js'
+import Header from './Header.js'
+import ImageList from './ImageList.js'
+import './App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-
+        <header className='header'>
+          <Header />
+        </header>
+        <ImageList />
       </div>
     )
   }
---
+++ src/ImageList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ImageItem from './ImageItem.js'
+import images from './data.js'
+
+export default class ImageList extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    images.map(image =>
+                        <ImageItem
+                            caption={image.title}
+                        />)
+                }
+
+            </div>
+        )
+    }
+}
